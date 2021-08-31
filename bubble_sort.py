da8d08b694bfc372ad5a3cd17c00c7b9d125cb3d
Arvind Rao <47332035+arvind73@users.noreply.github.com>
2021-08-31 16:25:21 +0530

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,26 @@
+#comparison of two consecutive elements (first with second element) takes place.
+#Loop it for n-1 times where n is total number of elements.
+# you can use this to sort strings too
+def bubble_sort(elements):
+    size = len(elements)
+
+    for i in range(size-1):
+        swapped = False
+        for j in range(size-1-i):
+            if elements[j] > elements[j+1]:
+                tmp = elements[j]
+                elements[j] = elements[j+1]
+                elements[j+1] = tmp
+                swapped = True
+
+        if not swapped:
+            break
+
+
+if __name__ == '__main__':
+    elements = [5,9,2,1,67,34,88,34]
+    elements = [1,2,3,4,2]
+    elements = ["Ashwin", "Arjun", "Ritwik", "Vandana", "Karthik"]
+
+    bubble_sort(elements)
+    print(elements)  #['Arjun', 'Ashwin', 'Karthik', 'Ritwik', 'Vandana']
